239ad72d09cdc085328cb8428057c6bd667bc6ac
jaehyeon <jaehyeon0825@gmail.com>
2022-04-01 00:11:24 +0700

    finish before finish star text

--- app/src/main/java/com/example/examapp/MainActivity.kt
+++ app/src/main/java/com/example/examapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.examapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.examapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +15,20 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        popUpButton()
 
     }
     private fun popUpButton() {
         binding.mainPopButton.setOnClickListener {
+            Log.d("msg", "버튼 눌림")
+            // 객체 생서
+            val dialog = EvaluationDialog(this)
+            dialog.myDia()
 
         }
     }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
